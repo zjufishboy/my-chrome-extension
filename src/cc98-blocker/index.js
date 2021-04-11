5045b7cc9b96961010b7a77f116d73ac5314aa2e
zjufishboy <3170101504@zju.edu.cn>
2021-04-11 21:48:56 +0800

    feat: CC98和谐化处理

--- src/cc98-blocker/index.js
+++ src/cc98-blocker/index.js
@@ -18,12 +18,12 @@ if (!blockInfoStr) {
 }
 let currentName = "";
 
-const div = document.createElement("div");
-div.className = "wtf-cc98-block-button-group";
+const divGroup = document.createElement("div");
+divGroup.className = "wtf-cc98-block-button-group";
 const divButton = document.createElement("button");
 divButton.innerHTML = "拉黑";
 divButton.className = "wtf-cc98-block-button";
-div.appendChild(divButton);
+divGroup.appendChild(divButton);
 
 const add_block_name = () => {
   blockInfo.block_username_list.push(currentName);
@@ -31,7 +31,7 @@ const add_block_name = () => {
 };
 
 const hideMenu = () => {
-  div.setAttribute("style", "display: none;");
+  divGroup.setAttribute("style", "display: none;");
 };
 
 divButton.addEventListener("click", () => {
@@ -41,7 +41,7 @@ divButton.addEventListener("click", () => {
 });
 
 const showMenu = (x, y, name) => {
-  div.setAttribute("style", `display:flex;top:${y}px;left:${x}px`);
+  divGroup.setAttribute("style", `display:flex;top:${y}px;left:${x}px`);
   currentName = name;
   if (blockInfo.block_username_list.includes(currentName)) {
     divButton.innerHTML = "我也不知道为啥会出现";
@@ -50,7 +50,7 @@ const showMenu = (x, y, name) => {
   }
 };
 
-document.body.appendChild(div);
+document.body.appendChild(divGroup);
 document.addEventListener("contextmenu", (e) => {
   if (window.location.pathname !== "/newTopics") {
     return;
